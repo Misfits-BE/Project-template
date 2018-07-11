9d79cf39dec8f9f0c6b0e46c568b21c49d37ab3d
Tim Joosten <Tjoosten3@gmail.com>
2018-07-11 22:11:53 +0200

    Initial commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,203 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}" dir="ltr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <meta http-equiv="Content-Language" content="en" />
+        <meta name="msapplication-TileColor" content="#2d89ef">
+        <meta name="theme-color" content="#4188c9">
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"/>
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="mobile-web-app-capable" content="yes">
+        <meta name="HandheldFriendly" content="True">
+        <meta name="MobileOptimized" content="320">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <link rel="icon" href="./favicon.ico" type="image/x-icon"/>
+        <link rel="shortcut icon" type="image/x-icon" href="./favicon.ico" />
+        
+        <title>{{ asset('app.name') }}</title>
+        
+        {{-- Javascripts --}}
+        <script src="{{ asset('js/app.js') }}" async></script>
+
+        {{-- Stylesheets --}}
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,300i,400,400i,500,500i,600,600i,700,700i&amp;subset=latin-ext">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet" />
+    </head>
+    <body>
+    <div class="page" id="app">
+        <div class="page-main">
+            <div class="header py-4">
+                <div class="container">
+                    <div class="d-flex">
+                        <a class="header-brand" href="{{ url('/') }}">
+                            <img src="{{ asset('images/brand.svg') }}" class="header-brand-img" alt="{{ config('app.name') }} logo">
+                        </a>
+                
+                        <div class="d-flex order-lg-2 ml-auto">
+                            @if (auth()->check())
+                                <div class="dropdown d-none d-md-flex">
+                                    <a class="nav-link icon" data-toggle="dropdown">
+                                        <i class="fe fe-bell"></i>
+                                        <span class="nav-unread"></span>
+                                    </a>
+                                    <div class="dropdown-menu dropdown-menu-right dropdown-menu-arrow">
+                                        <a href="#" class="dropdown-item d-flex">
+                                            <span class="avatar mr-3 align-self-center" style="background-image: url(demo/faces/male/41.jpg)"></span>
+                                            
+                                            <div>
+                                                <strong>Nathan</strong> pushed new commit: Fix page load performance issue.
+                                                <div class="small text-muted">10 minutes ago</div>
+                                            </div>
+                                        </a>
+
+                                        <div class="dropdown-divider"></div>
+                                        <a href="#" class="dropdown-item text-center text-muted-dark">Mark all as read</a>
+                                    </div>
+                                </div>
+                
+                                <div class="dropdown">
+                                    <a href="#" class="nav-link pr-0 leading-none" data-toggle="dropdown">
+                                        <span class="avatar" style="background-image: url(./demo/faces/female/25.jpg)"></span>
+                                        <span class="ml-2 d-none d-lg-block">
+                                        
+                                        <span class="text-default">Jane Pearson</span>
+                                            <small class="text-muted d-block mt-1">Administrator</small>
+                                        </span>
+                                    </a>
+                  
+                                    <div class="dropdown-menu dropdown-menu-right dropdown-menu-arrow">
+                                        <a class="dropdown-item" href="#">
+                                            <i class="dropdown-icon fe fe-settings"></i> Settings
+                                        </a>
+                    
+                                        <div class="dropdown-divider"></div>
+                    
+                                        <a class="dropdown-item" href="#">
+                                            <i class="dropdown-icon fe fe-help-circle"></i> Need help?
+                                        </a>
+                                        
+                                        <a class="dropdown-item" href="#">
+                                            <i class="dropdown-icon fe fe-log-out"></i> Sign out
+                                        </a>
+                                    </div>
+                                </div>
+                            @endif
+                        </div>
+
+                        <a href="#" class="header-toggler d-lg-none ml-3 ml-lg-0" data-toggle="collapse" data-target="#headerMenuCollapse">
+                            <span class="header-toggler-icon"></span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+           <div class="header collapse d-lg-flex p-0" id="headerMenuCollapse">
+	            <div class="container">
+		            <div class="row align-items-center">
+			            <div class="col-lg-3 ml-auto">
+				            <form class="input-icon my-3 my-lg-0">
+					            <input type="search" class="form-control header-search" placeholder="Search petition…" tabindex="1">
+					            <div class="input-icon-addon">
+						            <i class="fe fe-search"></i>
+					            </div>
+				            </form>
+			            </div>
+			            
+                        <div class="col-lg order-lg-first">
+				            <ul class="nav nav-tabs border-0 flex-column flex-lg-row">
+	                            <li class="nav-item">
+		                            <a href="{{ url('/') }}" class="nav-link"><i class="fe fe-home"></i> Home</a>
+	                            </li>
+                                <li class="nav-item">
+                                    <a href="" class="nav-link"><i class="fe fe-file-text"></i> Petitions</a>
+                                </li>
+                                
+                                @if (! auth()->check())
+                                    <li class="nav-item">
+                                        <a href="{{ route('register') }}" class="nav-link"><i class="fe fe-user-plus"></i> Register</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="{{ route('login') }}" class="nav-link"><i class="fe fe-user"></i> Login </a>
+                                    </li>
+                                @endif
+	                        </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="my-3 my-md-5">
+                @yield('content')
+            </div>
+        </div>
+
+        <div class="footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8">
+                        <div class="row">
+                            <div class="col-6 col-md-3">
+                                <ul class="list-unstyled mb-0">
+                                    <li><a href="#">First link</a></li>
+                                    <li><a href="#">Second link</a></li>
+                                </ul>
+                            </div>
+                            <div class="col-6 col-md-3">
+                                <ul class="list-unstyled mb-0">
+                                    <li><a href="#">Third link</a></li>
+                                    <li><a href="#">Fourth link</a></li>
+                                </ul>
+                            </div>
+                            <div class="col-6 col-md-3">
+                                <ul class="list-unstyled mb-0">
+                                    <li><a href="#">Fifth link</a></li>
+                                    <li><a href="#">Sixth link</a></li>
+                                </ul>
+                            </div>
+                            <div class="col-6 col-md-3">
+                                <ul class="list-unstyled mb-0">
+                                    <li><a href="#">Other link</a></li>
+                                    <li><a href="#">Last link</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 mt-4 mt-lg-0">
+                        Lorem ipsum dolor sit amet, et amet vitae fringilla dolor. Vel morbi blandit sed ipsumo.
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <footer class="footer">
+            <div class="container">
+                <div class="row align-items-center flex-row-reverse">
+                    <div class="col-auto ml-lg-auto">
+                        <div class="row align-items-center">
+                            <div class="col-auto">
+                                <ul class="list-inline list-inline-dots mb-0">
+                                    <li class="list-inline-item"><a href="./docs/index.html">Terms</a></li>
+                                    <li class="list-inline-item"><a href="./faq.html">Privacy Policy</a></li>
+                                </ul>
+                            </div>
+                            <div class="col-auto">
+                                <a href="https://github.com/tabler/tabler" class="btn btn-outline-primary btn-sm">
+                                    <i class="fe fe-chevrons-up"></i> Back to top
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-lg-auto mt-3 mt-lg-0 text-center">
+                        Copyright © {{ date('Y') }} <a href=".">{{ config('app.name') }}</a>. All rights reserved.
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </div>
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,254 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+             <div class="page-header mt-0 mb-1">
+                    <h1 class="page-title">Popular petition</h1>
+                </div>
+
+                <div class="card card-aside" style="min-height: 250px;">
+	
+
+	
+	<a href="#" class="card-aside-column" style="background-image: url(http://via.placeholder.com/350x150)"></a>
+	
+
+
+
+	<div class="card-body d-flex flex-column">
+		<h4><a href="#">And this isn't my nose. This is a false one.</a></h4>
+
+		<div class="text-muted">Look, my liege! The Knights Who Say Ni demand a sacrifice! …Are you suggesting that coconuts migr...</div>
+
+		<div class="d-flex align-items-center pt-5 mt-auto">
+			<div class="avatar avatar-md mr-3" style="background-image: url('http://via.placeholder.com/350x150')"></div>
+			<div>
+				<a href="./profile.html" class="text-default">Rose Bradley</a>
+				<small class="d-block text-muted">3 days ago</small>
+			</div>
+			<div class="ml-auto text-muted">
+				<a href="javascript:void(0)" class="icon d-none d-md-inline-block ml-3"><i class="fe fe-heart mr-1"></i></a>
+			</div>
+		</div>
+	</div>
+</div>
+            </div>
+
+            <div class="col-lg-9">
+                <div class="page-header mt-0 mb-1">
+                    <h1 class="page-title">Recent petitions</h1>
+                </div>
+
+                <div class="row row-cards row-deck">
+
+                    <div class="col-md-6">
+       <div class="card">
+	<div class="card-body d-flex flex-column">
+		<h4><a href="#">Weaseling out of things is important to learn.</a></h4>
+
+		<div class="text-muted">Please do not offer my god a peanut. That's why I love elementary school, Edna. The children beli...</div>
+
+		<div class="d-flex align-items-center pt-5 mt-auto">
+			<div class="avatar avatar-md mr-3" style="background-image: url(./demo/faces/male/4.jpg)"></div>
+			<div>
+				<a href="./profile.html" class="text-default">Bobby Knight</a>
+				<small class="d-block text-muted">3 days ago</small>
+			</div>
+			<div class="ml-auto text-muted">
+				<a href="javascript:void(0)" class="icon d-none d-md-inline-block ml-3"><i class="fe fe-heart mr-1"></i></a>
+			</div>
+		</div>
+	</div>
+</div>
+    </div><div class="col-md-6">
+       <div class="card">
+	<div class="card-body d-flex flex-column">
+		<h4><a href="#">Weaseling out of things is important to learn.</a></h4>
+
+		<div class="text-muted">Please do not offer my god a peanut. That's why I love elementary school, Edna. The children beli...</div>
+
+		<div class="d-flex align-items-center pt-5 mt-auto">
+			<div class="avatar avatar-md mr-3" style="background-image: url(./demo/faces/male/4.jpg)"></div>
+			<div>
+				<a href="./profile.html" class="text-default">Bobby Knight</a>
+				<small class="d-block text-muted">3 days ago</small>
+			</div>
+			<div class="ml-auto text-muted">
+				<a href="javascript:void(0)" class="icon d-none d-md-inline-block ml-3"><i class="fe fe-heart mr-1"></i></a>
+			</div>
+		</div>
+	</div>
+</div>
+    </div><div class="col-md-6">
+       <div class="card">
+	<div class="card-body d-flex flex-column">
+		<h4><a href="#">Weaseling out of things is important to learn.</a></h4>
+
+		<div class="text-muted">Please do not offer my god a peanut. That's why I love elementary school, Edna. The children beli...</div>
+
+		<div class="d-flex align-items-center pt-5 mt-auto">
+			<div class="avatar avatar-md mr-3 shadow-sm" style="background-image: url(./demo/faces/male/4.jpg)"></div>
+			<div>
+				<a href="./profile.html" class="text-default">Bobby Knight</a>
+				<small class="d-block text-muted">3 days ago</small>
+			</div>
+			<div class="ml-auto text-muted">
+            <a href="javascript:void(0)" class="icon d-none d-md-inline-block ml-3"><i class="fe fe-heart mr-1"></i> 200</a>
+				<a href="javascript:void(0)" class="icon d-none d-md-inline-block ml-3"><i class="fe fe-heart mr-1"></i> 200</a>
+			</div>
+		</div>
+	</div>
+</div>
+    </div><div class="col-md-6">
+       <div class="card">
+	<div class="card-body d-flex flex-column">
+		<h4><a href="#">Weaseling out of things is important to learn.</a></h4>
+
+		<div class="text-muted">Please do not offer my god a peanut. That's why I love elementary school, Edna. The children beli...</div>
+
+		<div class="d-flex align-items-center pt-5 mt-auto">
+			<div class="avatar avatar-md mr-3 shadow-sm" style="background-image: url(./demo/faces/male/4.jpg)"></div>
+			<div>
+				<a href="./profile.html" class="text-default">Bobby Knight</a>
+				<small class="d-block text-muted">3 days ago</small>
+			</div>
+			<div class="ml-auto text-muted">
+            <a href="javascript:void(0)" class="icon d-none d-md-inline-block ml-3"><i class="fe fe-heart mr-1"></i> 200</a>
+				<a href="javascript:void(0)" class="icon d-none d-md-inline-block ml-3"><i class="fe fe-heart mr-1"></i> 200</a>
+			</div>
+		</div>
+	</div>
+</div>
+    </div><div class="col-md-6">
+       <div class="card">
+	<div class="card-body d-flex flex-column">
+		<h4><a href="#">Weaseling out of things is important to learn.</a></h4>
+
+		<div class="text-muted">Please do not offer my god a peanut. That's why I love elementary school, Edna. The children beli...</div>
+
+		<div class="d-flex align-items-center pt-5 mt-auto">
+			<div class="avatar avatar-md mr-3 shadow-sm" style="background-image: url(./demo/faces/male/4.jpg)"></div>
+			<div>
+				<a href="./profile.html" class="text-default">Bobby Knight</a>
+				<small class="d-block text-muted">3 days ago</small>
+			</div>
+			<div class="ml-auto text-muted">
+            <a href="javascript:void(0)" class="icon d-none d-md-inline-block ml-3"><i class="fe fe-heart mr-1"></i> 200</a>
+				<a href="javascript:void(0)" class="icon d-none d-md-inline-block ml-3"><i class="fe fe-heart mr-1"></i> 200</a>
+			</div>
+		</div>
+	</div>
+</div>
+    </div><div class="col-md-6">
+       <div class="card">
+	<div class="card-body d-flex flex-column">
+		<h4><a href="#">Weaseling out of things is important to learn.</a></h4>
+
+		<div class="text-muted">Please do not offer my god a peanut. That's why I love elementary school, Edna. The children beli...</div>
+
+		<div class="d-flex align-items-center pt-5 mt-auto">
+			<div class="avatar avatar-md mr-3 shadow-sm" style="background-image: url(./demo/faces/male/4.jpg)"></div>
+			<div>
+				<a href="./profile.html" class="text-default">Bobby Knight</a>
+				<small class="d-block text-muted">3 days ago</small>
+			</div>
+			<div class="ml-auto text-muted">
+            <a href="javascript:void(0)" class="icon d-none d-md-inline-block ml-3"><i class="fe fe-heart mr-1"></i> 200</a>
+				<a href="javascript:void(0)" class="icon d-none d-md-inline-block ml-3"><i class="fe fe-heart mr-1"></i> 200</a>
+			</div>
+		</div>
+	</div>
+</div>
+    </div><div class="col-md-6">
+       <div class="card">
+	<div class="card-body d-flex flex-column">
+		<h4><a href="#">Weaseling out of things is important to learn.</a></h4>
+
+		<div class="text-muted">Please do not offer my god a peanut. That's why I love elementary school, Edna. The children beli...</div>
+
+		<div class="d-flex align-items-center pt-5 mt-auto">
+			<div class="avatar avatar-md mr-3 shadow-sm" style="background-image: url(./demo/faces/male/4.jpg)"></div>
+			<div>
+				<a href="./profile.html" class="text-default">Bobby Knight</a>
+				<small class="d-block text-muted">3 days ago</small>
+			</div>
+			<div class="ml-auto text-muted">
+            <a href="javascript:void(0)" class="icon d-none d-md-inline-block ml-3"><i class="fe fe-heart mr-1"></i> 200</a>
+				<a href="javascript:void(0)" class="icon d-none d-md-inline-block ml-3"><i class="fe fe-heart mr-1"></i> 200</a>
+			</div>
+		</div>
+	</div>
+</div>
+    </div><div class="col-md-6">
+       <div class="card">
+	<div class="card-body d-flex flex-column">
+		<h4><a href="#">Weaseling out of things is important to learn.</a></h4>
+
+		<div class="text-muted">Please do not offer my god a peanut. That's why I love elementary school, Edna. The children beli...</div>
+
+		<div class="d-flex align-items-center pt-5 mt-auto">
+			<div class="avatar avatar-md mr-3" style="background-image: url(./demo/faces/male/4.jpg)"></div>
+			<div>
+				<a href="./profile.html" class="text-default">Bobby Knight</a>
+				<small class="d-block text-muted">3 days ago</small>
+			</div>
+			<div class="ml-auto text-muted">
+				<a href="javascript:void(0)" class="icon d-none d-md-inline-block ml-3"><i class="fe fe-heart mr-1"></i></a>
+			</div>
+		</div>
+	</div>
+</div>
+    </div>
+
+                </div>
+
+                <a href=""class="btn btn-primary btn-lg btn-block">View more petitions</a>
+            </div>
+
+            <div class="col-lg-3 order-lg-1 mb-4">
+
+			<div class="list-group list-group-transparent mb-0">
+				
+				
+				<a href="../docs/index.html" class="list-group-item list-group-item-action disabled"><h4 class="mb-0">Topics</h4></a>
+				
+			</div>
+			
+			<!-- Components -->
+			<div class="list-group list-group-transparent mb-0">
+				
+				
+				<a href="../docs/alerts.html" class="list-group-item list-group-item-action"><span class="icon mr-3"><i class="fe fe-alert-triangle"></i></span>Alerts</a>
+				
+				
+				<a href="../docs/avatars.html" class="list-group-item list-group-item-action"><span class="icon mr-3"><i class="fe fe-user"></i></span>Avatars</a>
+				
+				
+				<a href="../docs/buttons.html" class="list-group-item list-group-item-action"><span class="icon mr-3"><i class="fe fe-plus-square"></i></span>Buttons</a>
+				
+				
+				<a href="../docs/colors.html" class="list-group-item list-group-item-action"><span class="icon mr-3"><i class="fe fe-feather"></i></span>Colors</a>
+				
+				
+				<a href="../docs/cards.html" class="list-group-item list-group-item-action"><span class="icon mr-3"><i class="fe fe-image"></i></span>Cards</a>
+				
+				
+				<a href="../docs/charts.html" class="list-group-item list-group-item-action"><span class="icon mr-3"><i class="fe fe-pie-chart"></i></span>Charts</a>
+				
+				
+				<a href="../docs/form-components.html" class="list-group-item list-group-item-action"><span class="icon mr-3"><i class="fe fe-check-square"></i></span>Form components</a>
+				
+				
+				<a href="../docs/tags.html" class="list-group-item list-group-item-action"><span class="icon mr-3"><i class="fe fe-tag"></i></span>Tags</a>
+				
+				
+				<a href="../docs/typography.html" class="list-group-item list-group-item-action"><span class="icon mr-3"><i class="fe fe-type"></i></span>Typography</a>
+				
+			</div>
+		
+		</div>
+        </div>
+    </div>
+@endsection
