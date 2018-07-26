a73d65ac84297164b441b4653fcbd3370df87735
Tim Joosten <Tjoosten3@gmail.com>
2018-07-26 15:58:37 +0200

    Implement sidenav partial.

---
+++ resources/views/account/partials/sidenav.blade.php
@@ -0,0 +1,19 @@
+<div class="list-group list-group-transparent mb-0">
+    <a href="#" class="list-group-item list-group-item-action disabled">
+        <h4 class="align-left mb-0">Account settings</h4>
+    </a>
+
+    <a href="{{ route('account.info') }}" class="list-group-item active list-group-item-action">
+        <span class="icon mr-3"><i class="fe fe-user"></i></span>
+        Account information
+    </a>
+
+    <a href="" class="list-group-item list-group-item-action">
+        <span class="icon mr-3"><i class="fe fe-lock"></i></span>
+        Account security
+    </a>
+
+    <a href="" class="list-group-item list-group-item-action">
+        <span class="icon mr-3"><i class="fe fe-x-circle"></i></span> Delete account
+    </a>
+</div>
--- resources/views/account/settings-info.blade.php
+++ resources/views/account/settings-info.blade.php
@@ -87,25 +87,7 @@
             </div>
 
             <div class="col-lg-3 order-lg-1 mb-4"> {{-- Sidenav --}}
-                <div class="list-group list-group-transparent mb-0">
-                    <a href="#" class="list-group-item list-group-item-action disabled">
-                        <h4 class="align-left mb-0">Account settings</h4>
-                    </a>
-
-                    <a href="{{ route('account.info') }}" class="list-group-item active list-group-item-action">
-                        <span class="icon mr-3"><i class="fe fe-user"></i></span>
-                        Account information
-                    </a>
-
-                    <a href="" class="list-group-item list-group-item-action">
-                        <span class="icon mr-3"><i class="fe fe-lock"></i></span>
-                        Account security
-                    </a>
-
-                    <a href="" class="list-group-item list-group-item-action">
-                        <span class="icon mr-3"><i class="fe fe-x-circle"></i></span> Delete account
-                    </a>
-                </div>
+                @include('account.partials.sidenav')
             </div> {{-- /// Sidenav --}}
         </div>
     </div>
