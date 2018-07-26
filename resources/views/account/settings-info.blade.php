d0eec6f46998fce5f01ba62d81d50821871fc215
Tim Joosten <Tjoosten3@gmail.com>
2018-07-26 14:18:36 +0200

    Complete user settings info view.

--- resources/views/account/settings-info.blade.php
+++ resources/views/account/settings-info.blade.php
@@ -21,42 +21,56 @@
                                 </div>
                             </div>
 
-                            <div class="col-sm-5">
+                            <div class="col-sm-6 col-sm-6">
                                 <div class="form-group">
-                        <label class="form-label">Date of birth</label>
-                        <div class="row gutters-xs">
-                            <div class="col-5">
-                                <select name="user[month]" class="form-control custom-select">
-                                    <option value="">Month</option>
-                                    <option value="1">January</option>
-                                    <option value="2">February</option>
-                                    <option value="3">March</option>
-                                    <option value="4">April</option>
-                                    <option value="5">May</option>
-                                    <option value="6">June</option>
-                                    <option value="7">July</option>
-                                    <option value="8">August</option>
-                                    <option value="9">September</option>
-                                    <option value="10">October</option>
-                                    <option value="11">November</option>
-                                    <option value="12">December</option>
-                                </select>
+                                    <label class="form-label">Date of birth</label>
+                                    <div class="row gutters-xs">
+                                        <div class="col-5">
+                                            <select name="user[month]" class="form-control custom-select">
+                                                <option value="">Month</option>
+                                                <option value="1">January</option>
+                                                <option value="2">February</option>
+                                                <option value="3">March</option>
+                                                <option value="4">April</option>
+                                                <option value="5">May</option>
+                                                <option value="6">June</option>
+                                                <option value="7">July</option>
+                                                <option value="8">August</option>
+                                                <option value="9">September</option>
+                                                <option value="10">October</option>
+                                                <option value="11">November</option>
+                                                <option value="12">December</option>
+                                            </select>
+                                        </div>
+                                        <div class="col-3">
+                                            <select name="user[day]" class="form-control custom-select">
+                                                <option value="">Day</option>
+                                            </select>
+                                        </div>
+                                        <div class="col-4">
+                                            <select name="user[year]" class="form-control custom-select">
+                                                <option value="">Year</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
-                            <div class="col-3">
-                                <select name="user[day]" class="form-control custom-select">
-                                <option value="">Day</option>
-                                </select>
+
+                            <div class="col-md-6 col-sm-6">
+                                <div class="form-group">
+                                    <div class="form-label">Username: <span class="text-danger">*</span></div>
+                                    <input type="text" class="form-control">
+                                </div>
                             </div>
-                            <div class="col-4">
-                                <select name="user[year]" class="form-control custom-select">
-                                <option value="">Year</option>
-                                </select>
+
+                            <div class="col-md-12 col-sm-12">
+                                <div class="form-group">
+                                    <div class="form-label">E-mail address: <span class="text-danger">*</span></div>
+                                    <input type="text" class="form-control">
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </div>
-            </div>
-                      
+
                         <div class="card-footer text-right pr-0 pb-1">
                             <button type="submit" class="btn btn-primary">Update Profile</button>
                         </div>
@@ -66,7 +80,7 @@
 
             <div class="col-lg-3 order-lg-1 mb-4"> {{-- Sidenav --}}
                 <div class="list-group list-group-transparent mb-0">
-				    <a href="#" class="list-group-item list-group-item-action disabled">
+                    <a href="#" class="list-group-item list-group-item-action disabled">
                         <h4 class="align-left mb-0">Account settings</h4>
                     </a>
 
@@ -83,7 +97,7 @@
                     <a href="" class="list-group-item list-group-item-action">
                         <span class="icon mr-3"><i class="fe fe-x-circle"></i></span> Delete account
                     </a>
-			    </div>
+                </div>
             </div> {{-- /// Sidenav --}}
         </div>
     </div>
