3907562ba7cae4040df1d302f9cde621aff2bc0e
Tjoosten <Topairy@gmail.com>
2018-07-27 23:38:33 +0200

    Provide user logout method

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -77,9 +77,13 @@
                     
                                         <div class="dropdown-divider"></div>
 
-                                        <a class="dropdown-item" href="#">
+                                        <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                                             <i class="dropdown-icon fe fe-log-out"></i> Sign out
                                         </a>
+
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                            @csrf {{-- Form field protection --}}
+                                        </form>
                                     </div>
                                 </div>
                             @endif
